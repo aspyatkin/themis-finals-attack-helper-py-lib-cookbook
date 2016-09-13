bcdac060e436dc15a8fab590633ad56c198a8e62
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-13 17:22:37 +0400

    Draft

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+id = 'themis-finals-attack-helper-py-lib'
+
+default[id]['basedir'] = '/var/themis/finals/lib/themis-finals-attack-helper-py'
+default[id]['user'] = 'vagrant'
+default[id]['group'] = 'vagrant'
+default[id]['github_repository'] = 'aspyatkin/themis-finals-attack-helper-py'
+default[id]['revision'] = 'master'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'themis-finals-attack-helper-py-lib'
+description 'Installs and configures themis.finals.attack.helper package'
+version '1.0.0'
+
+recipe 'themis-finals-attack-helper-py-lib',
+       'Installs and configures themis.finals.attack.helper package'
+depends 'latest-git', '~> 1.1.11'
+depends 'git2', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 1.0.0'
